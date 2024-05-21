97b6b7425e454f5cc2b008c2c644e5a0db2f566f
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2024-05-21 00:26:42 -0300

    clase1

---
+++ CLASES/Clase 1/linterna.js
@@ -0,0 +1,44 @@
+
+let estado_linterna = "apagado";
+
+/* capturo el sonido */
+let sonido_batman = document.querySelector("#bati_encendido");
+let sonido_click = document.querySelector ("#bati_click");
+
+/* capturar las cajas */
+
+let batman = document.querySelector(".batman");
+let bati_boton = document.querySelector(".bati_boton");
+
+/* evento */
+bati_boton.addEventListener("click",controlar_linterna)
+
+/* funcion controlar linterna */
+
+function controlar_linterna () {
+if (estado_linterna=="apagado"){
+    estado_linterna="encendido";
+    sonido ()
+batman.classList.add ("batman_activo")
+}else{
+    estado_linterna = "apagado"
+    sonido ()
+    batman.classList.remove("batman_activo")
+}
+}
+
+/* funcion para manipular el sonido */
+
+function sonido () {
+    if(sonido_batman.paused){
+        sonido_click.play()
+        sonido_batman.play()
+    }else{
+        sonido_click.play()
+        sonido_batman.pause()
+}
+}
+
+
+
+
