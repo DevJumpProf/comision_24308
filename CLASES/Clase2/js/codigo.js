0e12870f0b4c7eff51a338f8d388384f0fd73833
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2024-05-22 22:30:03 -0300

    js

---
+++ CLASES/Clase2/js/codigo.js
@@ -0,0 +1,102 @@
+/* let nombre = "Nicolino"
+const apellido = "Locche"
+
+console.log(nombre);
+console.log(apellido); */
+
+/*  nombre = "Gustavo"
+ let nombre = "Gustavo"
+let Nombre = "Gustavo" */
+/* apellido = "Ballas" */
+
+/* console.log(nombre); */
+
+
+/* let ingreso = confirm ("Queres ingresar?")
+let nombre = prompt ("Ingresá tu nombre") */
+
+/* alert ("ingresá un numero " + nombre +" " + " Gracias") */
+/* alert (`ingresa un numero ${nombre}`) */
+
+/* let num1= prompt ("Ingresá un numero") */  /* 10 */
+/* let num2= prompt ("Ingresá otro numero") *//*  2 */
+/* prompt por defecto = String
+10+10 */
+/* alert(`${nombre} el resultado de tu suma es ${num1*num2}`) */
+/* 
+let num1= parseInt(prompt ("Ingresá un numero") )
+let num2= parseInt(prompt ("Ingresá otro numero")) */
+
+/* let num1= Number(prompt ("Ingresá un numero") )
+let num2= Number(prompt ("Ingresá otro numero"))
+ */
+
+/* let dato = Number(false)
+console.log(dato);
+ */
+/* parseInt - parseFloat */
+
+/*  document.write(num1+num2) 
+ */
+
+/* FUNCIONES DECLARATIVAS*/ 
+
+
+/* function suma (num1,num2){
+    document.write (num1+num2)
+}
+suma (10,10)
+ */
+/* suma (Number(prompt ("Ingresá un numero")),Number(prompt ("Ingresá otro numero"))) */
+
+/* FUNCIONES FLECHA - ARROW FUNCTION */
+
+/* const suma = (num1,num2) =>{return (num1+num2)};console.log(suma (10,10)); 
+
+const saludo = nombre=>`Hola ${nombre}`;console.log(saludo("Alicia"));
+
+const sumar = (num1,num2) =>num1+num2;console.log(sumar (10,10));  */
+
+
+/* condicional if */
+
+let edad = 8
+
+if (edad>=18){
+   console.log("Podes Pasar");
+}else {
+    console.log("NO Podes Pasar");
+}
+
+// IF TERNARIO
+
+/* CONDICION ? TRUE : FALSE */
+
+edad>=18 ?console.log("Podes Pasar") : console.log("NO Podes Pasar");
+
+
+/* CALLBACK */
+
+/* const calculadora = (num1,num2,operacion) => operacion (num1,num2)
+
+const sumar = (num1,num2) => num1+num2
+const restar = (num1,num2) => num1-num2
+const multiplicar = (num1,num2) => num1*num2
+const dividir = (num1,num2) => num1/num2
+
+console.log(calculadora(10,10,sumar));
+console.log(calculadora(10,10,restar));
+console.log(calculadora(10,10,multiplicar));
+ */
+
+
+
+
+
+
+
+
+
+
+
+
